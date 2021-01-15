74e83fd590a847dfa876a2ba86b12d9f1c6da3dd
Emily <yongldich@gmail.com>
2021-01-15 14:59:22 -0800

    adding fetch template

---
+++ README.md
@@ -0,0 +1,9 @@
+# Getting started
+1. Clone the repository using Git client or commandline ```git clone ...```
+2. Navigate into repository folder ```cd fetch_api_template```
+3. run ```npm install``` in directory where package.json is (in our case, just in the root folder aka where you just navigated into)
+4. Open package.json and you'll see a script command ```node fetch_api.js``` connected to easy script: ```npm fetch```. You can change the script call and the file path (i.e. if fetch_api.js is in a folder called 'src', then the command is: ```node src/fetch_api.js```)
+5. If you haven't already, run ```npm run fetch```, you should get a json back. 
+
+# Editting fetch_api.js
+1. There is a `url` variable that you can replace the endpoint with your endpoint choice
---
+++ fetch_api.js
@@ -0,0 +1,78 @@
+// import "dotenv/config";
+const fetch = require("node-fetch");
+const fs = require("fs");
+console.log("Hello Project.");
+
+const token = "b53896ad4f17b48225121956ae2c7aab489e002a";
+// uncomment below if you want to write to file, you can use the fs library 
+
+// const write_file = "sample.json";
+// let write_stream = fs.createWriteStream(write_file);
+// write_stream
+//     .on("finish", () => {
+//         console.log("finished writing data.");
+//     })
+//     .write("[");
+// async function fetching(areas) {
+//     for (let i = 0; i < areas.length; i++) {
+//         await fetchEndpoint();
+//     }
+//     write_stream.write("]");
+// }
+
+const url = `https://api.github.com/users/yonglid`
+console.log("fetching: ", fetchEndpoint(url))
+async function fetchEndpoint(fetchUrl) {
+    // let fetchedEndpoint = 
+    fetch(
+        // some requests don't need you to pass a header/don't need authorization and you can just hit the endpoint for a simple GET 
+        `${fetchUrl}`
+
+        // to pass in more parameters to the fetch function like Headers, Body etc, reference the Fetch documentation, but here are some examples below which you can tack on if you had a comma after the above endpoint url 
+        // also reference this useful blog with examples: https://danlevy.net/you-may-not-need-axios/#custom-headers
+
+        // 1) some requests might just need a really simple header, one below is one provided by github 
+        // {
+        //     "headers": {
+        //         "accept": "application/vnd.github.inertia-preview+json",
+        //         "method": "GET"
+        //     }
+        // }
+
+        // 2) some requests don't need you to pass a header, but you might need to send a token or a more detailed header and here's an example (base it off of the documentation's headers)
+        // {
+        //   "headers": {
+        //     "accept": "application/json, text/plain, */*",
+        //     "accept-language": "en-US,en;q=0.9",
+        //     "authorization": `Token ${token}`, // the `` syntax allows you to reference variables using ${} so here we're bringing in the value of a variable named token 
+        //     "sec-fetch-dest": "empty",
+        //     "sec-fetch-mode": "cors",
+        //     "sec-fetch-site": "same-origin",
+        //     "cookie": "G_ENABLED_IDPS=google; G_AUTHUSER_H=0"
+        //     //   cookie: "G_ENABLED_IDPS=google; G_AUTHUSER_H=2",
+        //   },
+        //   "referrer": "some refferer link",
+        //   "referrerPolicy": "no-referrer-when-downgrade",
+        //   "body": null,
+        //   "method": "GET",
+        //   "mode": "cors",
+        // }
+
+        // 3) for POSTs, you'd probably want to send a header and body based on the documentation 
+        // headers: {
+        //     Accept: 'application/json',
+        //     'Content-Type': 'application/json',
+        //   },
+        // body: JSON.stringify({
+        // username: "some user",
+        // password: "password",
+        // }),
+    ).then(response =>
+        response.json()
+    ).then(data =>
+        console.log(data)
+    )
+        .catch(err => console.log("Error fetching endpoint: " + err));
+    // let data = await fetchedEndpoint.json()
+
+}
